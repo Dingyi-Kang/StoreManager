6059d874b75ab4aa138a4334b9b12e26edf6ee97
Dingyi-Kang <dkang@okstate.edu>
2021-11-19 19:05:31 -0600

    not worked well

---
+++ Item+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Item+CoreDataClass.swift
+//  StoreManager
+//
+//  Created by DingyiKang on 11/19/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Item)
+public class Item: NSManagedObject {
+
+}
